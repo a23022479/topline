4a3e32b1df1791360e65c7b11d06c2dc72509b6f
a23022479 <964101978@qq.com>
2019-10-07 19:46:48 +0800

    头条项目创建

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+
+// 导入element-ui的样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.config.productionTip = false
+// 使用element-ui
+Vue.use(ElementUI)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
